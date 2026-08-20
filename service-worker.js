39b2f73d9ae3e6784b3692f0b808ffdf57d40ea1
Shankh <flavio.naretti@gmail.com>
2026-08-20 18:55:32 +0100

    Aggiungo PIN corretto-errato

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gioco-delle-case-v7-adminpin-audio-ios-20260820";
+const CACHE_NAME = "gioco-delle-case-v7-1-pin-feedback-20260820";
 const APP_SHELL = [
   "./",
   "./index.html",
